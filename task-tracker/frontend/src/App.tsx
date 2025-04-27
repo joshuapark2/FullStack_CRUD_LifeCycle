04407621e36a5060b42fb857863beaa2f6aa354a
Joshua Park <joshuapark1309@gmail.com>
2025-04-26 20:59:34 -0400

    GET and POST Built Out

--- task-tracker/frontend/src/App.tsx
+++ task-tracker/frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 
--- task-tracker/frontend/src/components/TaskList.tsx
+++ task-tracker/frontend/src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../redux/store";
 import type { Task } from "../types/Task";
